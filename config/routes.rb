9435676d6da167d06a781259cdeaf01fc28525cf
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-07 22:59:10 +0900

    本の編集処理作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # resources :books, only: [:index]
   namespace :api do
-    resources :books, only: [:index, :show, :create]
+    resources :books, only: [:index, :show, :create, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
