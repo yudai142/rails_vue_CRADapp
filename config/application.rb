907c304fd8ff94b9799e2d378161a48509d88235
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-07 11:50:21 +0900

    foreman導入

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.x.webpacker[:dev_server_host] = "http://localhost:8080"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
