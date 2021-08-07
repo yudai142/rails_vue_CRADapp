d01ed94f6637141e3f274fa6fc8d14d0097b6db6
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-07 22:38:38 +0900

    訂正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
   root to: 'home#index'
   # resources :books, only: [:index]
-  get 'home/index'
   namespace :api do
-    resources :books, only: [:index, :show]
+    resources :books, only: [:index, :show, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
