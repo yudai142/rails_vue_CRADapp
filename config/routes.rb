fd00cdf0bb20219a4026564330433a3206bcf207
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-08-07 23:03:10 +0900

    削除処理作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # resources :books, only: [:index]
   namespace :api do
-    resources :books, only: [:index, :show, :create, :update]
+    resources :books, only: [:index, :show, :create, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
